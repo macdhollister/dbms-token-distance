6b1edc91cb312aa3aab07783d726655e5b76a826
macdhollister <macdhollister@gmail.com>
2022-05-27 00:58:12 -0700

    Adding hooks for hovering over tokens; other tokens get distance via speech bubble

--- scripts/token-distance.js
+++ scripts/token-distance.js
@@ -1,19 +1,32 @@
 // TODO: clean this up
-let selected = canvas.tokens.controlled[0]
-canvas.tokens.objects.children.filter(tok => tok.id != selected.id).forEach(other => {
 
-	let first = {
-		x: selected.x,
-		y: selected.y
-	}
 
-	let second = {
-		x: other.x,
-		y: other.y
-	}
+function getOtherTokens(token) {
+    return canvas.tokens.objects.children.filter(tok => tok.id != token.id)
+}
 
-	let distance = canvas.grid.measureDistance(first,second, {"gridSpaces": true})
-	let roundedDistance = Math.round(distance * 100) / 100
+function calculateTokenDistance(token1, token2) {
+    let first = {
+        x: token1.x,
+        y: token1.y
+    }
 
-	canvas.hud.bubbles.say(other, roundedDistance)
-})
+    let second = {
+        x: token2.x,
+        y: token2.y
+    }
+
+    let distance = canvas.grid.measureDistance(first, second, {"gridSpaces": true})
+    let roundedDistance = Math.round(distance * 100) / 100
+
+    return roundedDistance
+}
+
+Hooks.on("hoverToken", (hoveredToken, isHovered) => {
+    let state = isHovered ? "hovered" : "unhovered";
+
+    if(state == "hovered") getOtherTokens(hoveredToken).forEach(other => {
+        let roundedDistance = calculateTokenDistance(hoveredToken, other)
+        canvas.hud.bubbles.say(other, roundedDistance)
+    })
+});
